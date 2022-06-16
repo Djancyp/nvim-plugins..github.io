062ed25ca58f8d5994c0789219423ec0be550b21
Djan <youcyprus@gmail.com>
2022-06-16 11:27:38 +0200

    Add fonts and style

--- components/searchbar/search-bar.vue
+++ components/searchbar/search-bar.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="search-box w-full mt-10 flex flex-row relative">
-    <input
-      ref="search"
-      v-model="search"
-      class="search border-2 border-2 border-gray-200 shadow-xl"
-      type="text"
-      placeholder="Search Plugin"
-    />
+    <div class="flex justify-items-center items-center relative">
+      <font-awesome-icon :icon="['fa', 'fa-search']" class="text-gray-500 p-4 absolute" />
+      <input
+        ref="search"
+        v-model="search"
+        class="search border-2 border-2 border-gray-200 shadow-xl px-10"
+        type="text"
+        placeholder="Search Plugin"
+      />
+    </div>
     <ul class="pt-5 list-disc text-blue-600 flex flex-row text-sm font-bold">
       <li class="mx-4">Plugin Name</li>
       <li class="mx-4">Plugin Tags</li>
@@ -45,7 +48,6 @@ export default {
   height: 3em;
   border: 1px solid #ccc;
   border-radius: 5px;
-  padding: 0.5em;
   font-size: 1.2em;
 }
 .search:focus {
